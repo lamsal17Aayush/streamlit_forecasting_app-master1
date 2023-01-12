103f549bb653293b90507f57dee9a076ad61b0f7
Lamsal Aayush <tryhackaayush@gmail.com>
2023-01-12 11:50:44 +0545

    committed

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-sudo apt install streamlit
 import streamlit as st 
 import pandas as pd
 import numpy as np 
 from prophet import Prophet 
-from prophet.diagonistics import performance_metrics , cross_validation
+from prophet.diagnostics import performance_metrics
+from prophet.diagnostics import cross_validation
 from prophet.plot import plot_cross_validation_metric
 import base64
 
@@ -36,3 +36,39 @@ min_value = 1,max_value=365)
 if df is not None:
     m = Prophet()
     m.fit(data)
+    
+# Step 3: Visualize forecast data 
+# The below visual shows future predicted values. "yhat" is the predicted 
+
+if df is not None:
+    future = m.make_future_dataframe(periods=periods_input)
+    forecast = m.predict(future)
+    fcst = forecast(['ds','yhat_lower','yhat_upper'])
+    fcst_filtered= fcst[fcst['ds']>max_date]
+    st.write(fcst_filtered)
+    
+    
+    """ The next visual shows the actual (black dots) and predicted 
+    """
+    fig1 = m.plot(forecast)
+    st.write(fig1)
+    
+    
+    fig2 = m.plot_components(forecast)
+    st.write(fig2)
+""" 
+if df is not None:
+    csv_exp=fcst_filtered.to_csv(index=False)
+    
+    
+    b64=base64.b64encode(csv_exp.encode()).decode() 
+    href=f'<a href="data:file/csv;base64,{b64}">Download CSV file</a>  (right-click and save as ** &lt;forecast_name&ft;.csv***)'
+    st.markdown(href,unsafe_allow_html=True)
+"""  
+    
+    
+    
+    
+    
+    
+    
